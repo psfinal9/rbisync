e857c363516df91bd59b7852c8db271ad498e7fa
Mobile_Jason <psfinal@mobile>
2022-07-08 13:52:23 +0900

    1. add argument : specific exclude file, dry-run(not working yet) 2. check sync status to exit asap 3. check "bisyc failure", "bisync abort" 4. minor display fix

--- rbisync.sh
+++ rbisync.sh
@@ -1,12 +1,60 @@
 #!/bin/bash
 
-PATH1=$1
-PATH2=$2
+PATH1=''
+PATH2=''
+
+DRY_RUN=false
+EXCLUDE_FILE=''
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -d|--dry-run)
+      DRY_RUN=true
+      shift # past argument
+      shift # past value
+      ;;
+    -e|--exclude)
+      SPECIFIC_EXCLUDE_FILE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+PATH1=${POSITIONAL_ARGS[0]}
+PATH2=${POSITIONAL_ARGS[1]}
+
+#
+# To-Do: 
+# dry-run
+#
 
 #echo "Bisync $PATH1 and $PATH2 ..."
 BISYNC_LOG="bisync.log"
 
 EXCLUDE_FILE="exclude.list"
+
+if [ -e $EXCLUDE_FILE ]; then
+  rm $EXCLUDE_FILE
+  touch $EXCLUDE_FILE
+fi
+
+if [ -n $SPECIFIC_EXCLUDE_FILE ] ; then
+    echo $SPECIFIC_EXCLUDE_FILE > $EXCLUDE_FILE
+fi
+RESULT_REPORT_TO_JOPLIN=233
+RESULT_REPORT_NOTHING=0
+
 # store bisync status log with --dry-run into bisync.log
 # for debugging
 # rclone bisync $PATH1 $PATH2 --dry-run --verbose --exclude _backup/** --log-format="" 2>&1 | $tee BISYNC_LOG
@@ -15,26 +63,42 @@ if [ -e $BISYNC_LOG ]; then
   rm $BISYNC_LOG
 fi
 
-rclone bisync $PATH1 $PATH2 --dry-run --verbose --exclude _backup/** --log-format="" --log-file=$BISYNC_LOG
+#
+# -1. trying to find No changes foun
+#
 
+rclone bisync $PATH1 $PATH2 --dry-run --verbose --exclude _backup/** --exclude-from $EXCLUDE_FILE --log-format="" --log-file=$BISYNC_LOG
+
+DRY_RUN=$(cat $BISYNC_LOG | sed -s 's/:/ /'|grep -E 'No changes found')
+RES=(${DRY_RUN})
+
+if [ ${#RES[@]} -gt 0 ]; then 
+    echo "## No update exit ##"
+    cat $BISYNC_LOG 2>&1
+    #python -u ~/_NASSYNC/GitHub/python/Log2Joplin.py --path ~/ --notebook 데일리업뎃 2>&1 | tee -a checkMailDaily.log
+    exit $RESULT_REPORT_NOTHING
+fi
 
 #
 # 0.check if bisync failed or not
 # "Failed to bisync: all files were changed"
 #
-DRY_RUN=$(cat $BISYNC_LOG | sed -s 's/:/ /'|grep -E 'Failed to bisync')
+DRY_RUN=$(cat $BISYNC_LOG | sed -s 's/:/ /'|grep -E 'Failed to bisync|Bisync aborted')
 RES=(${DRY_RUN})
 
 if [ ${#RES[@]} -gt 0 ]; then 
     echo "## failed bisync...report to Joplin & exit ##"
-    cat $BISYNC_LOG
-    python -u ~/_NASSYNC/GitHub/python/Log2Joplin.py --path ~/ --notebook 데일리업뎃 2>&1 | tee -a checkMailDaily.log
-    exit 0
+    cat $BISYNC_LOG 2>&1
+    #python -u ~/_NASSYNC/GitHub/python/Log2Joplin.py --path ~/ --notebook 데일리업뎃 2>&1 | tee -a checkMailDaily.log
+    exit $RESULT_REPORT_TO_JOPLIN
 fi
 
 
 #
 # 1. capture "copy to" action from bisync.log
+# TO-DO: in case new file...no need to backup
+# error will be shown but works normally
+#
 DRY_RUN=$(cat $BISYNC_LOG | sed -s 's/:/ /'|grep -E 'INFO\s*-\s*Path[1|2]\s*Queue copy to'|awk '{\
 if ($7 == "Path2") {sub(".*/","",$9); filename=substr($0, index($0, $9)); gsub(/ /,"\\",filename); print "Path2 " filename;} \
 else if ($7 == "Path1") {sub(".*/","",$9); filename=substr($0, index($0, $9)); gsub(/ /,"\\",filename); print "Path1 " filename;} \
@@ -61,6 +125,12 @@ create_backup_folder() {
 	#echo "$1 IS REMOTE"
 		rclone mkdir "$1/$2"
 	else
+	
+	
+	if [ ! -d $1 ]; then
+	    mkdir $1
+	fi
+	
 		if [ ! -d $1/$2 ]; then
 			   mkdir "$1/$2"
 		fi
@@ -88,7 +158,7 @@ do
 	RES[$i+1]=${RES[$i+1]/\\/ }  
 	create_backup_folder $PATH1 $BACKUP_DIR_NAME
 	
-	echo "[$((($i+1)/2))/$((${#RES[@]}/2))] backup **Path1/${RES[$i+1]}** Path1/$BACKUP_DIR_NAME/"
+	echo "[$((($i+1)%2))/$((${#RES[@]}/2))] backup **Path1/${RES[$i+1]}** Path1/$BACKUP_DIR_NAME/"
 	if [[ "$PATH1" =~ ':' ]] ; then
 	    rclone copy $PATH1/"${RES[$i+1]}" $PATH1/$BACKUP_DIR_NAME/ 2>&1
 	else
@@ -100,7 +170,7 @@ do
 	RES[$i+1]=${RES[$i+1]/\\/ }
 	create_backup_folder $PATH2 $BACKUP_DIR_NAME
 	
-	echo "[$((($i+1)/2))/$((${#RES[@]}/2))] backup **Path2/${RES[$i+1]}** Path2/$BACKUP_DIR_NAME/"
+	echo "[$((($i+1)%2))/$((${#RES[@]}/2))] backup **Path2/${RES[$i+1]}** Path2/$BACKUP_DIR_NAME/"
 	if [[ "$PATH2" =~ ':' ]] ; then
 	    rclone copy $PATH2/"${RES[$i+1]}" $PATH2/$BACKUP_DIR_NAME/ 2>&1
 	else
@@ -147,7 +217,7 @@ do
 	RES[$i+1]=${RES[$i+1]/\\/ }  
 	create_backup_folder $PATH1 $BACKUP_DIR_NAME
 
-    echo "[$((($i+1)/2))/$((${#RES[@]}/2))]  backup **Path1/${RES[$i+1]}** Path1/$BACKUP_DIR_NAME"
+    echo "[$((($i+1)%2))/$((${#RES[@]}/2))]  backup **Path1/${RES[$i+1]}** Path1/$BACKUP_DIR_NAME"
 	if [[ "$PATH1" =~ ':' ]] ; then
 	   rclone copy $PATH1/"${RES[$i+1]}" $PATH1/$BACKUP_DIR_NAME/ 2>&1
 	else
@@ -160,7 +230,7 @@ do
 	RES[$i+1]=${RES[$i+1]/\\/ }  
 	create_backup_folder $PATH2 $BACKUP_DIR_NAME
 
-	echo "[$((($i+1)/2))/$((${#RES[@]}/2))]  backup **Path2/${RES[$i+1]}** Path2/$BACKUP_DIR_NAME/"
+	echo "[$((($i+1)%2))/$((${#RES[@]}/2))]  backup **Path2/${RES[$i+1]}** Path2/$BACKUP_DIR_NAME/"
     if [[ "$PATH2" =~ ':' ]] ; then
        rclone copy $PATH2/"${RES[$i+1]}" $PATH2/$BACKUP_DIR_NAME/ 2>&1
     else
@@ -181,10 +251,6 @@ DRY_RUN=$(cat $BISYNC_LOG | sed -s 's/:/ /'|grep -E 'NOTICE\s*-\s*WARNING\s*New 
 RES=(${DRY_RUN})
 
 # backup the copying files into path[1 or 2]/_backup if there are "copy to" jobs
-if [ -e $EXCLUDE_FILE ]; then
-  rm $EXCLUDE_FILE
-  touch $EXCLUDE_FILE
-fi
 	
 if [ ${#RES[@]} -gt 0 ]; then
 	echo "## ${#RES[@]} files conflict...will be excluded when bisyncing... ##"
@@ -197,11 +263,11 @@ if [ ${#RES[@]} -gt 0 ]; then
 		RES[$i]=${RES[$i]/\\/ }  
 		#EXCLUDE_FILE="--exclude \"${RES[$i]}\""
 		#EXCLUDE_FILE=${RES[$i]}
-		echo "[$((($i+1)/2))/${#RES[@]}] Exclude conflict file: **${RES[$i]}**"
+		echo "[$((($i+1)%2))/${#RES[@]}] Exclude conflict file: **${RES[$i]}**"
 		echo ${RES[$i]} >> $EXCLUDE_FILE
 	done
 else
-	echo "***No file conflict in both Path1/Path2 ...***"
+	echo "## No file conflict in both Path1/Path2 ... ##"
 fi
 echo ""
 
@@ -215,6 +281,7 @@ fi
 
 
 if $REPORT ; then
-   echo "Report to Joplin..."
-   python -u ~/_NASSYNC/GitHub/python/Log2Joplin.py --path ~/ --notebook 데일리업뎃 2>&1 | tee -a checkMailDaily.log
+   exit 233 # RESULT_REPORT_TO_JOPLIN
+else
+   exit 0 # no report
 fi
